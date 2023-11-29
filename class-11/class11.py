eacb3efe3a044e16930d9b0392996c26909a437b
Muhammad Hassaan Shaukat <mr.hassaan50@gmail.com>
2023-11-29 05:28:36 +0500

    Python Classes Update

---
+++ class-11/class11.py
@@ -0,0 +1,8 @@
+a : list[int] = [1, 2, 3, 4]
+
+b : list[int] = [1, 2, 3, 4]
+
+print(id(a))
+print(id(b))
+
+print(a + b)
---
+++ class-9/class9.py
@@ -0,0 +1,7 @@
+import sys
+
+print("Line 1")
+print("Line 2")
+
+print(sys.argv)
+
